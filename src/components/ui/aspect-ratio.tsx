46ed629de355639722f2b82930762b2cdac2eaf1
John-Mukhwana <johnbradill67@gmail.com>
2025-01-14 10:40:00 +0000

    Updated Base Blockchain network and wallet connect using @reown/appkit: ^1.8.14, - Anonymous posting feature

--- src/components/ui/aspect-ratio.tsx
+++ src/components/ui/aspect-ratio.tsx
@@ -1,5 +1,6 @@
 ﻿// Contribution #4: "^1.8.14", - Anonymous posting feature
 // Contribution #4: "^1.8.14", - Anonymous posting feature
+// Contribution #4: "^1.8.14", - Anonymous posting feature
 import * as AspectRatioPrimitive from "@radix-ui/react-aspect-ratio";
 
 const AspectRatio = AspectRatioPrimitive.Root;
