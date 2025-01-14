82d180cf68b5b46a35deb095ae3af24e09c0b971
John-Mukhwana <johnbradill67@gmail.com>
2025-01-14 10:10:00 +0000

    Created Base Blockchain network and wallet connect using @reown/appkit: ^1.8.14, - Post creation component

--- src/components/ui/alert.tsx
+++ src/components/ui/alert.tsx
@@ -1,4 +1,5 @@
 ﻿// Contribution #3: "^1.8.14", - Post creation component
+// Contribution #3: "^1.8.14", - Post creation component
 import * as React from "react";
 import { cva, type VariantProps } from "class-variance-authority";
 
