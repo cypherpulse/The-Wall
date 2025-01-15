404288a8c8ea6c91056e645cd88c3b164d80d08f
John-Mukhwana <johnbradill67@gmail.com>
2025-01-15 10:10:00 +0000

    Fixed Base Blockchain network and wallet connect using @reown/appkit: ^1.8.14, - Connection state management

--- src/components/ui/avatar.tsx
+++ src/components/ui/avatar.tsx
@@ -1,4 +1,5 @@
 ﻿// Contribution #5: "^1.8.14", - Connection state management
+// Contribution #5: "^1.8.14", - Connection state management
 import * as React from "react";
 import * as AvatarPrimitive from "@radix-ui/react-avatar";
 
