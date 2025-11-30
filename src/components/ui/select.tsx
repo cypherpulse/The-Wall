539f83e5bd5711661bacefcc7ba925b698a7ceb8
John-Mukhwana <johnbradill67@gmail.com>
2025-11-30 17:40:00 +0000

    Created Base Blockchain network and wallet connect using @reown/appkit: ^1.8.14, - Voting mechanism

--- src/components/ui/select.tsx
+++ src/components/ui/select.tsx
@@ -1,3 +1,4 @@
+﻿// Contribution #33: "^1.8.14", - Voting mechanism
 import * as React from "react";
 import * as SelectPrimitive from "@radix-ui/react-select";
 import { Check, ChevronDown, ChevronUp } from "lucide-react";
